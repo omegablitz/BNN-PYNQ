8513309733a95acc8a90aec138c8cf3cf08302f3
Aashish Welling <aashishwelling@gmail.com>
2019-05-04 20:42:54 -0400

    Fix build script

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build . --tag build-bnn && docker run -it --mount type=bind,src=/home/aashish/Work/MIT/6.888/project/Xilinx,dst=/app/Xilinx --mount type=volume,src=build,dst=/app/persist build-bnn
+#!/bin/bash
+docker build . --tag build-bnn && docker run -it --mount type=bind,src=/home/aashish/Work/MIT/6.888/project/Xilinx,dst=/app/Xilinx --mount type=bind,src=/home/aashish/Work/MIT/6.888/project/6.888-final-project/BNN-PYNQ/build,dst=/app/persist build-bnn
